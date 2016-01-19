c5dbafc36012ce79d8e9e8bc1872cc87a6248b2d
Liu Xinan <xinan@me.com>
2016-01-19 23:23:51 +0800

    Add `Interest` model

---
+++ app/models/interest.rb
@@ -0,0 +1,3 @@
+class Interest < ActiveRecord::Base
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119145414) do
+ActiveRecord::Schema.define(version: 20160119152252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "interests", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
