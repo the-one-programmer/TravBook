886f2d90ebd63c80d59e65f062478c1841ab87f4
Liu Xinan <xinan@me.com>
2016-01-20 00:02:24 +0800

    Add `City` model

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,5 @@
 class Country < ActiveRecord::Base
   validates :name, presence: true
+
+  has_many :cities
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119152252) do
+ActiveRecord::Schema.define(version: 20160119160047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "country_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
