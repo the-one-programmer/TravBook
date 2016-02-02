95b60ae438a2fd0a774fa3e59eaad8edc6362245
harryggg <ma.tanghao@dhs.sg>
2016-02-03 02:16:54 +0800

    reset password email url

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "dont_reply@travbook.me"
 
   def reset_password_email(user, token)
     @user = user
-    @url  = 'localhost:3000/reset?token=' + token
+    @url  = 'localhost:3000/resetpassword?token=' + token
     mail(to: @user.email, subject: 'Reset your password!')
   end
 end
